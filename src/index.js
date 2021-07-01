08d0ea5ce9379b8bb4a9b483235b72434cbd73e8
Eri Perdomo <eriloperdomod1588@gmail.com>
2021-07-01 08:58:53 -0500

    Primera parte codigo HTML y CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const tarjeta = document.querySelector('#tarjeta'),
+      btnabrirformulario = document.querySelector('#btn-abrir-formulario'),
+      formulario = document.querySelector('formulario-tarjeta');
+
+tarjeta.addEventListener('click', () => {
+    tarjeta.classList.toggle('active');
+});
+
+btnabrirformulario.addEventListener('click', () => {
+    btnabrirformulario.classList.toggle('active');
+    formulario.classList.toggle('active');
+});
